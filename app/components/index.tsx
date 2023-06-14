72a28ef555648301db76c6e02c7178d868713fb2
valyndsilva <valyndsilva@gmail.com>
2023-06-14 17:03:31 +0100

    Create component:MainView

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -5,3 +5,4 @@ export { default as Sidebar } from "./Sidebar";
 export { default as PodcastCard } from "./PodcastCard";
 export { default as PodcastDetail } from "./PodcastDetail";
 export { default as EpisodeDetail } from "./EpisodeDetail";
+export { default as MainView } from "./MainView";
